cc848300f2cd63f09eea76452de4f383c721c8b3
Manuel Altalef <maltalef101@gmail.com>
2020-08-15 20:00:55 -0300

    no

--- minux.sh
+++ minux.sh
@@ -54,7 +54,7 @@ checkUser() {
 }
 
 preInstallConfirm() {
-  dialog --title "-- Minux installation --" --yes-label "Yes, continue." --no-label "No! Go back" --yesno "This is your last chance to back out.\n\nDo you wish to continue with the installation?" 10 60 || exit 1
+  dialog --title "-- Minux installation --" --yes-label "Yes, continue." --no-label "No! Go back" --yesno "This is your last chance to back out.\n\nDo you wish to continue with the installation?" 10 60
 }
 
 addUser() {
@@ -141,7 +141,7 @@ getUserAndPass
 checkUser
 
 # prompts the user for confirmation on the installation. this is the user's last chance to back out.
-preInstallConfirm
+preInstallConfirm || exit 1
 
 # actually adds the entered username and password
 addUser "$username"
