5fc0a3c0024c4be25e94cebb48e5513189503f56
Manuel Altalef <maltalef101@gmail.com>
2020-09-20 22:33:39 -0300

    Fix mistakes that made script unusable

--- minux.sh
+++ minux.sh
@@ -49,14 +49,14 @@ getUserAndPass() {
 }
 
 checkUser() {
-  if [ "$(id -u $username 2>&1)" !=1 ]; then
+  if [ "$(id -u $username >/dev/null 2>&1; echo $?)" != 1 ]; then
     dialog --title "-- Minux installation --" --yes-label "Yes, continue." --no-label "No! Go back" --yesno "User '$username' already exists, if you continue, your login credentials will be overwritten, as well as your config files if you then chose to continue with the installation.\n\nDo you wish to continue?" 10 60
   fi
 }
 
 preInstallConfirm() {
     dialog --title "-- Minux installation --" --yes-label "Yes, continue." --no-label "No! Go back" --yesno "This is your last chance to back out.\n\nDo you wish to continue with the installation?" 10 60
-    if [ "echo $?" != 1 ]; then
+    if [ "echo $?" == 1 ]; then
         exit 1
     fi
 }
